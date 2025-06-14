4f312474ae1559162195345268fadd967372d8e3
mirzafar <m_mirzafar@mail.ru>
2025-06-14 12:22:36 +0500

    f

--- core/cache.py
+++ core/cache.py
@@ -1,11 +1,7 @@
 import functools
-import traceback
 from typing import Optional, Union
 
-import aio_pika
 import aioredis
-import ujson
-from aio_pika.abc import AbstractRobustChannel
 from aioredis import Redis
 
 from settings import settings
@@ -16,7 +12,7 @@ class Cache:
     def __init__(self):
         self.pool: Optional[Redis] = None
         self.connection = None
-        self.channel: Optional[AbstractRobustChannel] = None
+        # self.channel: Optional[AbstractRobustChannel] = None
 
     async def initialize(self, loop):
         self.pool = await aioredis.from_url(
@@ -32,22 +28,22 @@ class Cache:
         #
         # self.channel = await self.connection.channel()
 
-    async def queue(self, queue, **kwargs):
-        if self.channel is None:
-            return
-
-        if self.channel.is_closed:
-            await self.channel.reopen()
-
-        try:
-            await self.channel.default_exchange.publish(
-                aio_pika.Message(
-                    body=bytes(ujson.dumps(kwargs), 'utf-8')
-                ),
-                routing_key=queue
-            )
-        except (Exception,):
-            traceback.print_exc()
+    # async def queue(self, queue, **kwargs):
+    #     if self.channel is None:
+    #         return
+    #
+    #     if self.channel.is_closed:
+    #         await self.channel.reopen()
+    #
+    #     try:
+    #         await self.channel.default_exchange.publish(
+    #             aio_pika.Message(
+    #                 body=bytes(ujson.dumps(kwargs), 'utf-8')
+    #             ),
+    #             routing_key=queue
+    #         )
+    #     except (Exception,):
+    #         traceback.print_exc()
 
     def __getattr__(self, attr):
         return functools.partial(getattr(self.pool, attr))
